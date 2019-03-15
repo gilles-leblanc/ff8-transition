abda7153502fc533dd46a6bb0d20100bcd97ef78
Gilles Leblanc <gilles.leblanc@gmail.com>
2019-03-14 21:25:56 -0400

    Add possibility to limit animation speed

--- transition.js
+++ transition.js
@@ -7,8 +7,9 @@ const buffer = [];
 const _numberOfIndicesPerPixel = 4;
 
 const config = {
-  horizontalSegments: 10,
   direction: SideEnum.left,  
+  horizontalSegments: 10,
+  frameLimitFactor: 1,        // 1 is normal speed
 }
 
 var calcValueToAddLeft = function(x, tenth) {
@@ -20,25 +21,27 @@ var calcValueToAddRight = function(x, tenth) {
 }
 
 var whithen = function(timestamp) {
-  let t0 = performance.now();
+  const lastValueToCheck = config.direction === SideEnum.right ? 0 : imageData.data.length - 1;
+  if (Math.floor(timestamp % config.frameLimitFactor) === 0) {
+    let t0 = performance.now();
 
-  const width = imageData.width * _numberOfIndicesPerPixel;  
-  const height = imageData.height;
-  const tenth = width / config.horizontalSegments;
-  const lastValueToCheck = config.direction === SideEnum.left ? 0 : imageData.data.length - 1;
-  const calcValueToAddFunc = config.direction === SideEnum.left ? calcValueToAddLeft : calcValueToAddRight;
-
-  for (let y = 0; y < height; y++) {      
-    for (let x = 0; x < width; x++) {      
-      const valueToAdd = calcValueToAddFunc(x, tenth);
-
-      imageData.data[y * width + x] += valueToAdd;      
+    const width = imageData.width * _numberOfIndicesPerPixel;  
+    const height = imageData.height;
+    const tenth = width / config.horizontalSegments;    
+    const calcValueToAddFunc = config.direction === SideEnum.left ? calcValueToAddLeft : calcValueToAddRight;
+  
+    for (let y = 0; y < height; y++) {      
+      for (let x = 0; x < width; x++) {      
+        const valueToAdd = calcValueToAddFunc(x, tenth);
+  
+        imageData.data[y * width + x] += valueToAdd;      
+      }
     }
-  }
-
-  context.putImageData(imageData, 0, 0);
-  let t1 = performance.now();
-  console.log((t1 - t0) + " milliseconds.");
+  
+    context.putImageData(imageData, 0, 0);
+    let t1 = performance.now();
+    console.log((t1 - t0) + " milliseconds.");
+  }   
 
   if (imageData.data[lastValueToCheck] < 250) {
     window.requestAnimationFrame(whithen); 
@@ -57,10 +60,6 @@ window.addEventListener("load", function() {
     window.requestAnimationFrame(whithen);
   };
   
-  // turn screen to white
-    // pick left or right side
-    // from starting side to other side, starting side gets brighter faster from starting side up until white
-
   // when almost all white, start transforming to black instantly on the same side and each line at different speeed goes black to the opposing side
     // start black at about 80% to 90% done on each line. 
     // all lines do not go to same speed
