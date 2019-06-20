5a0772691817bc00330b1c780efba12c3e6a299a
Gilles Leblanc <gilles.leblanc@gmail.com>
2019-06-19 22:09:11 -0400

    Fix issue when transition starting from the right side

--- transition.js
+++ transition.js
@@ -12,7 +12,7 @@ const offColor = 0;
 let height, width, segmentLength, sideMultiplier, initialX;
 
 const config = {
-  direction: SideEnum.right,  
+  direction: SideEnum.left,  
   frameLimitFactor: 1,        
   horizontalSegments: 10,
   initSpread: 0.2,
@@ -27,7 +27,8 @@ var initTransition = function() {
   for (let y = 0; y < height; y++) {      
     let numberOfPixelsToInit = Math.floor(Math.random() * Math.floor(spread));
     xProgress.push(numberOfPixelsToInit);    
-    for (let x = 0; x < numberOfPixelsToInit; x++) {
+
+    for (let x = 0; x <= numberOfPixelsToInit; x++) {
       imageData.data[y * width + (initialX + x * sideMultiplier)] = config.color;
     }    
   }
@@ -42,12 +43,12 @@ var colorSwoosh = function(timestamp) {
     }
     
     for (let y = 0; y < height; y++) {      
-      for (let x = xProgress[y]; x < width; x++) {      
+      for (let x = xProgress[y]; x <= width; x++) {      
         const valueToAdd = calcValueToAddFunc(x, segmentLength + xProgress[y]);
-        imageData.data[y * width + (initialX + x * sideMultiplier)] += valueToAdd * passMultiplier;      
+        imageData.data[y * width + (initialX + x * sideMultiplier)] += valueToAdd * passMultiplier;
       }
     }
-  
+    
     context.putImageData(imageData, 0, 0);
   }
 
@@ -55,8 +56,7 @@ var colorSwoosh = function(timestamp) {
     color(config.currentPass === Pass.first ? 1 : -1);    
   }   
   
-  const lastValueToCheck = config.direction === SideEnum.right ? 0 : imageData.data.length - 1;
-  // TODO: right side doesn't work
+  const lastValueToCheck = config.direction === SideEnum.right ? 1 : imageData.data.length - 1;
 
   if (config.currentPass === Pass.first) {
     if (imageData.data[lastValueToCheck] < config.color) {
@@ -83,13 +83,12 @@ window.addEventListener("load", function() {
   
     context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
-    console.log(imageData);
 
     width = imageData.width * numberOfIndicesPerPixel;  
     height = imageData.height;
     segmentLength = width / config.horizontalSegments;
     sideMultiplier = config.direction === SideEnum.left ? 1 : -1;
-    initialX = config.direction === SideEnum.left ? 0 : width;  
+    initialX = config.direction === SideEnum.left ? 0 : width - 1;  
 
     initTransition();
     window.requestAnimationFrame(colorSwoosh);
