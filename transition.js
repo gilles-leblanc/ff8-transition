b429389a79877ce029524083c8afb47457a974a3
Gilles Leblanc <gilles.leblanc@gmail.com>
2019-07-31 21:52:14 -0400

    Fix transition to work in both directions
    Also add Click me! label to make things more obvious.

--- transition.js
+++ transition.js
@@ -12,7 +12,7 @@ const offColor = 0;
 let height, width, segmentLength, sideMultiplier, initialX;
 
 const config = {
-  direction: SideEnum.right,  
+  direction: SideEnum.left,  
   frameLimitFactor: 1,        
   horizontalSegments: 10,
   initSpread: 0.2,
@@ -56,7 +56,7 @@ var colorSwoosh = function(timestamp) {
     color(config.currentPass === Pass.first ? 1 : -1);    
   }   
   
-  const lastValueToCheck = config.direction === SideEnum.right ? 1 : imageData.data.length - 1;
+  const lastValueToCheck = config.direction === SideEnum.right ? 1 : imageData.data.length - numberOfIndicesPerPixel;
 
   if (config.currentPass === Pass.first) {
     if (imageData.data[lastValueToCheck] < config.color) {
@@ -77,15 +77,20 @@ var colorSwoosh = function(timestamp) {
 
 window.addEventListener("load", function() {
   canvas = document.getElementById('main-canvas');
-  canvas.onclick = function() {
-    context = canvas.getContext('2d', { alpha: false });
+  context = canvas.getContext('2d', { alpha: false });
+  
+  context.fillStyle = '#ffffff';
+  context.font = 'bold 30px sans-serif';
+  context.textBaseline = 'bottom';
+  context.fillText('Click me!', canvas.width / 2 - 30, canvas.height / 2 - 30);
 
+  canvas.onclick = function() {   
     context.drawImage(document.getElementById('ff8'), 0, 0);
     imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 
-    // if (Math.floor(Math.random() * 2) === 0) {
-    //   config.direction = SideEnum.right;
-    // }
+    if (Math.floor(Math.random() * 2) === 0) {
+      config.direction = SideEnum.right;
+    }
 
     width = imageData.width * numberOfIndicesPerPixel;  
     height = imageData.height;
